0e4bcbe3a44944af47f6b3c7a0892bd900c7fe37
mamy1310 <mamitiana_rakotoarisoa@yahoo.com>
2021-05-13 12:07:05 +0300

    Configure for heroku server

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const http = require('http');
+let express = require('express');
+let app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-access-token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+let port = process.env.PORT || 8010;
+
+// On démarre le serveur
+app.listen(port, "0.0.0.0");
+console.log('Serveur démarré sur http://localhost:' + port);
+
+module.exports = app;
