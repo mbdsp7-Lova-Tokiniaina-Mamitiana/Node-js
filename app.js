49eeb5130b67d6c4f94766394ba8c56b5045bc99
TokiniainaHerve <tokiniainaherve.andrianarison@gmail.com>
2021-05-15 11:15:28 +0300

    jwt connection

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 let express = require('express');
 let app = express();
+var config = require('./config/config');
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+let bodyParser = require('body-parser');
+let user = require('./routes/users');
 
+let uri = config.dburi;
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+};
+
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log("Connecté à la base MongoDB assignments dans le cloud !");
+    console.log("at URI = " + uri);
+    console.log("vérifiez with http://localhost:8010/api/ que cela fonctionne")
+    },
+    err => {
+      console.log('Erreur de connexion: ', err);
+    });
+
+// Pour accepter les connexions cross-domain (CORS)
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-access-token");
@@ -9,8 +32,24 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Pour les formulaires
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 let port = process.env.PORT || 8010;
 
+// les routes
+const prefixUser = '/api/users';
+
+    // Users
+app.route(prefixUser + '/register')
+  .post(user.createUser);
+app.route(prefixUser + '/auth')
+  .get(user.connectUserByToken);
+app.route(prefixUser + '/login')
+  .post(user.login);
+app.route(prefixUser + '/logout')
+  .post(user.logout);
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
 console.log('Serveur démarré sur http://localhost:' + port);
---
+++ config/config.js
@@ -0,0 +1,5 @@
+const dbConnection='mongodb+srv://tptmbds:tptmbds@cluster0.op7ak.mongodb.net/dbpari?retryWrites=true&w=majority';
+module.exports = {
+    'secret': 'tptmbds',
+    'dburi' :dbConnection
+  };
